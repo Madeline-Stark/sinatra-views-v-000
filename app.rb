91b30b3b2553526511101c5023e5fb88dc841877
Madeline Stark <madeline.stark1@gmail.com>
2018-09-08 22:56:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,12 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index #tells Sinatra to render a file called index.erb
-	end
+		get '/' do
+			erb :index #tells Sinatra to render a file called index.erb
+		end
 
 
-		get "/info" do
+		get "/info" do #will show up as url plus /info
       "Testing the info page"
     end
 
